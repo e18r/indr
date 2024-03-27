8cafd2454f3742c547243fd2d042efc74b0af5e7
Emilio Silva Schlenker <e18r@disroot.org>
2024-03-26 21:28:58 -0500

    feat(publish): normalize, minus character filter

--- indr.go
+++ indr.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"strings"
+	//	"log"
 )
 
 type Palindrome struct {
 	Text string `json:"text"`
+}
+
+var ascii = map[string]string{
+  "á": "a",
+  "à": "a",
+  "â": "a",
+  "ä": "a",
+  "é": "e",
+  "è": "e",
+  "ê": "e",
+  "ë": "e",
+  "í": "i",
+  "ì": "i",
+  "î": "i",
+  "ï": "i",
+  "ó": "o",
+  "ò": "o",
+  "ô": "o",
+  "ö": "o",
+  "ú": "u",
+  "ù": "u",
+  "û": "u",
+  "ü": "u",
+}
+
+func normalize(text string) string {
+	text = strings.ToLower(text)
+	for chr, asciiChr := range(ascii) {
+		text = strings.ReplaceAll(text, chr, asciiChr)
+	}
+	return text
 }
 
 func main() {
@@ -22,7 +55,7 @@ func main() {
 			return c.Status(fiber.StatusBadRequest).
 				SendString("Text param not found in request body")
 		}
-		return nil
+		return c.SendString(normalize(palindrome.Text))
 	})
 
 	app.Listen(":3000")
