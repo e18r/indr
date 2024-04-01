427bd21f20ef270e0c1d91266d315327ca4baff7
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-01 17:20:43 -0500

    devops: env-sensitive deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 
 cd "$(dirname $0)"
 
-ENV=$(cat ./env)
+ENV=$(cat ./environment)
+PROJECT=$(jq -r .$ENV ./projects.json)
 DATABASE_URL="$(pass palindr/$ENV/db)"
 cat ./app.yaml.template | sed "s|\$DATABASE_URL|$DATABASE_URL|" > app.yaml
-# gcloud app deploy
-# shred -uzn99 ./app.yaml
+gcloud --project=$PROJECT app deploy
+shred -uzn99 ./app.yaml
+gcloud app logs tail -s default
