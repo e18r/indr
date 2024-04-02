8a2c6b75a257af59b25c5e7195a5406926485f88
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-01 20:18:46 -0500

    devops: watch.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@ cd "$(dirname $0)"
 
 ENV=$(cat ./environment)
 PROJECT=$(jq -r .$ENV ./projects.json)
-DATABASE_URL="$(pass palindr/$ENV/db)"
+DATABASE_URL="$(pass indr/$ENV/db)"
 cat ./app.yaml.template | sed "s|\$DATABASE_URL|$DATABASE_URL|" > app.yaml
 gcloud --project=$PROJECT app deploy
 shred -uzn99 ./app.yaml
-gcloud app logs tail -s default
