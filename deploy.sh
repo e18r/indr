58ddc29f631a50efd520edad05ab38fa00abfb96
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-02 22:33:30 -0500

    devops(deploy): log URL in deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,12 @@ echo "obtaining database url..."
 DATABASE_URL="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
                        | tail -n1 | xargs)"
 cat ./app.yaml.template | sed "s|\$DATABASE_URL|$DATABASE_URL|" > ./app.yaml
-if [ $1 = "dry" ]; then
+if [ "$1" = "dry" ]; then
     cat ./app.yaml
 else
     gcloud --project=$GCLOUD_PROJECT app deploy
 fi
 shred -uzn99 ./app.yaml
-./url.sh > ../pal/indr.url
+URL=$(./url.sh)
+echo $URL > ../pal/indr.url
+printf "\nURL: %s\nsaved in ../pal/indr.url\n" $URL
