8f8ad95e378afb504423f2889d2cedcfc85a2eca
Emilio Silva Schlenker <e18r@disroot.org>
2024-04-03 21:38:21 -0500

    fix(watch): get ENV before testing it

--- watch.sh
+++ watch.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname $0)"
 
+ENV=$(cat ./ENV)
 if [ "$ENV" != "test" -a "$ENV" != "prod" ]; then
     echo "only watch on test or prod envs"
     exit 1
 fi
 
-ENV=$(cat ./ENV)
 GCLOUD_PROJECT=$(jq -r .$ENV.project.gcloud ./settings.json)
 gcloud --project=$GCLOUD_PROJECT app logs tail -s default
