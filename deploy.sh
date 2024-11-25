da035f4abdb1eac554ad4da378277fd996b49066
Emilio Silva Schlenker <e18r@disroot.org>
2024-11-24 22:21:51 -0500

    devops(deploy): add neon db in a second env var

--- deploy.sh
+++ deploy.sh
@@ -17,13 +17,11 @@ GCLOUD_PROJECT=$(jq -r .$ENV.project.gcloud ./settings.json)
 HEROKU_PROJECT=$(jq -r .$ENV.project.heroku ./settings.json)
 NEON_PROJECT=$(jq -r .$ENV.project.neon ./settings.json)
 echo "obtaining database url..."
-if [ "$ENV" = "test" ]; then
-    DATABASE_URL="$(neon connection-string --project-id $NEON_PROJECT)"
-else
-    DATABASE_URL="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
+DATABASE_URL="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
                            | grep postgres | xargs)"
-fi
-cat ./app.yaml.template | sed "s|\$DATABASE_URL|$DATABASE_URL|" > ./app.yaml
+DATABASE_URL_2="$(neon connection-string --project-id $NEON_PROJECT)"
+cat ./app.yaml.template | sed "s|\$DATABASE_URL_2|$DATABASE_URL_2|" \
+    | sed "s|\$DATABASE_URL|$DATABASE_URL|" > ./app.yaml
 if [ "$1" = "dry" ]; then
     cat ./app.yaml
 else
