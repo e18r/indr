fd5746d4aa597fcf8b143cc3dada11229846cb50
Emilio Silva Schlenker <e18r@disroot.org>
2024-12-08 09:37:09 -0500

    devops(deploy): neon db as primary

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ GCLOUD_PROJECT=$(jq -r .$ENV.project.gcloud ./settings.json)
 HEROKU_PROJECT=$(jq -r .$ENV.project.heroku ./settings.json)
 NEON_PROJECT=$(jq -r .$ENV.project.neon ./settings.json)
 echo "obtaining database url..."
-DATABASE_URL="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
+DATABASE_URL_2="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
                            | grep postgres | xargs)"
-DATABASE_URL_2="$(neon connection-string --project-id $NEON_PROJECT)"
+DATABASE_URL="$(neon connection-string --project-id $NEON_PROJECT)"
 cat ./app.yaml.template | sed "s|\$DATABASE_URL_2|$DATABASE_URL_2|" \
     | sed "s|\$DATABASE_URL|$DATABASE_URL|" > ./app.yaml
 if [ "$1" = "dry" ]; then
