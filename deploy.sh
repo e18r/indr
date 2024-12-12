cfac5bc0c8ad05c14295346e1283c7a479aaf3ac
Emilio Silva Schlenker <e18r@disroot.org>
2024-12-12 15:38:38 -0500

    devops(deploy): disconnect heroku db

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ GCLOUD_PROJECT=$(jq -r .$ENV.project.gcloud ./settings.json)
 HEROKU_PROJECT=$(jq -r .$ENV.project.heroku ./settings.json)
 NEON_PROJECT=$(jq -r .$ENV.project.neon ./settings.json)
 echo "obtaining database url..."
-DATABASE_URL_2="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
-                           | grep postgres | xargs)"
+# DATABASE_URL_2="$(heroku pg:credentials:url -a $HEROKU_PROJECT \
+#                           | grep postgres | xargs)"
 DATABASE_URL="$(neon connection-string --project-id $NEON_PROJECT)"
 cat ./app.yaml.template | sed "s|\$DATABASE_URL_2|$DATABASE_URL_2|" \
     | sed "s|\$DATABASE_URL|$DATABASE_URL|" > ./app.yaml
